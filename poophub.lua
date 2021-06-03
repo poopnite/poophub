af8dcd44527d7b9955a3e830b6ca35727dfefb0c
poopnite <85274766+poopnite@users.noreply.github.com>
2021-06-02 22:47:15 -0700

    Update poophub.lua

--- poophub.lua
+++ poophub.lua
@@ -1,1 +1,2 @@
-if game.PlaceId == 4520749081 then loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/kinglegacy.lua"))()
+if game.PlaceId == 4520749081 then loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/kinglegacy.lua"))() 
+end
